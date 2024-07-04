d686c101a13f4c9ba14686e13b84c347a0368544
silena <sile7719@gmail.com>
2024-07-03 22:30:50 -0400

    First day of the challenge

---
+++ variable.js
@@ -0,0 +1,21 @@
+// Declaring different variables of different data types
+let firstName = 'Silena' // first name of a person
+let lastName = 'Delgado' // last name of a person
+let country = 'Paraguay' // country
+let city = 'Asunción' // capital city
+let age = 22 // age in years
+let isMarried = false
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Francisco', //name of a person
+  job = 'artist',
+  live = 'Paraguay'
+
+// First exercise
+let myAge = 28;  // Assigns my age, for example
+let yourAge = 22;  // Assigns another person's age, for example  
